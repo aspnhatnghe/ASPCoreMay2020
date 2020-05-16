15ed5902f649c18c4b5a5eb9a4460f45c5f49051
Hien <hyhien@gmail.com>
2020-05-16 15:33:27 +0700

    Code buoi 02

---
+++ Buoi02/Buoi02/Controllers/DemoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buoi02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi02.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LayMot()
+        {
+            //đọc csdl/file
+            var hh = new HangHoa
+            {
+                MaHangHoa = 1, 
+                TenHangHoa = "TOSHIBA",
+                DonGia = 179000
+            };
+
+            //đổ sang View hiển thị
+            return View(hh);
+        }
+
+        public IActionResult DanhSach()
+        {
+            List<HangHoa> danhsach = new List<HangHoa>();
+            HangHoa tmp;
+
+            var rd = new Random();
+            //sinh số phần tử ngẫu nhiên từ 3 -> 10
+            var soPhanTu = rd.Next(3, 11);
+
+            for(int i = 0; i < soPhanTu; i++)
+            {
+                tmp = new HangHoa
+                {
+                    MaHangHoa = i + 100,
+                    TenHangHoa = $"Samsung {rd.Next()}",
+                    DonGia = rd.NextDouble() * 10000000,
+                    GiamGia = rd.NextDouble()
+                };
+                danhsach.Add(tmp);//Thêm vào mảng
+            }
+
+            return View(danhsach);
+        }
+    }
+}
---
+++ Buoi02/Buoi02/Models/HangHoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi02.Models
+{
+    public class HangHoa
+    {
+        //Automatic Property
+        public int MaHangHoa { get; set; }
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+        public double GiamGia { get; set; }
+        public double GiaBan
+        {
+            get { 
+                return DonGia * (1 - GiamGia); 
+            }
+        }
+    }
+}
