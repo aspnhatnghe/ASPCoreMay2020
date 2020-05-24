6b891efb36a9365dcffda431f4b3c0faf89d23ba
NN <hyhien@gmail.com>
2020-05-24 08:16:35 +0700

    Day 05 - Init Project

---
+++ Buoi05_BootStrap/Buoi05_BootStrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buoi05_BootStrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ASPCoreMay2020
 ASP.NET Core 3.1 khai giảng 10.05.2020
+
+## Buổi 05 (24/05/2020): BootStrap
