cee17734b7208cb372c4223f144a774bed3145b4
Hien <hyhien@gmail.com>
2020-05-30 16:39:18 +0700

    Done calculator

--- Buoi06/Buoi06/Controllers/CalculatorController.cs
+++ Buoi06/Buoi06/Controllers/CalculatorController.cs
@@ -12,5 +12,31 @@ namespace Buoi06.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Calculate()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(double ToanHang1, double ToanHang2, string PhepToan)
+        {
+            double kq = 0;
+            switch(PhepToan)
+            {
+                case "+": kq = ToanHang1 + ToanHang2; break;
+                case "-": kq = ToanHang1 - ToanHang2; break;
+                case "^": kq = Math.Pow(ToanHang1, ToanHang2); break;
+            }
+
+            ViewBag.A = ToanHang1;
+            ViewBag.B = ToanHang2;
+            ViewBag.PhepToan = PhepToan;
+            ViewBag.KetQua = kq;
+
+            return View("Index");
+            //return Content($"{ToanHang1} {PhepToan} {ToanHang2}= {kq}");
+        }
     }
 }
