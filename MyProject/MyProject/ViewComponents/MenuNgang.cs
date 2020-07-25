75dea7634e8f477c889e5e29d82387688d6f0c79
NN <hyhien@gmail.com>
2020-07-25 15:52:33 +0700

    Done Menu Ngang

---
+++ MyProject/MyProject/ViewComponents/MenuNgang.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyProject.DataModels;
+using System.Threading.Tasks;
+
+namespace MyProject.ViewComponents
+{
+    public class MenuNgang : ViewComponent
+    {
+        private readonly MyDbContext DbContext;
+        public MenuNgang(MyDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("_MenuLoai", await DbContext.Loais.ToListAsync());
+        }
+    }
+}
