d7ab98bc582b12a72ee4360ce8f254fbb385e9f4
NN <hyhien@gmail.com>
2020-07-26 11:30:00 +0700

    Done Register account

---
+++ MyProject/MyProject/Controllers/KhachHangController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.DataModels;
+using MyProject.Helpers;
+using MyProject.ViewModels;
+using System;
+
+namespace MyProject.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public KhachHangController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        #region [Đăng ký]
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult DangKy(RegisterViewModel model)
+        {
+            var kh = new KhachHang
+            {
+                MaKhachHang = Guid.NewGuid().ToString(),
+                TenKhachHang = model.CustomerName,
+                DienThoai = model.Phone,
+                Email = model.Email,                
+                TrangThai = false,
+                RandomKey = MyTools.GetRandom(),
+                VaiTro = Common.Role.Customer
+            };
+            kh.MatKhau = model.Password.ToSHA512Hash(kh.RandomKey);
+            _context.Add(kh);
+            _context.SaveChanges();
+
+            try
+            {
+                var urlActive = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/KhachHang/ActiveAccount?id={kh.MaKhachHang}";
+                var noidungMail = @$"<div>
+            <i>Dear anh/chị <b>{kh.TenKhachHang}</i>
+<div>Bạn đã đăng ký thành công tài khoản tại .... Vui lòng click vào <a href={urlActive}>đây</a> để kích hoạt.</div>
+Trân trọng./.
+</div>";
+                GoogleMailer.Send(kh.Email, "Active tai khoan", noidungMail);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return Redirect("/HangHoa");
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyProject/MyProject/Helpers/GoogleMailer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace MyProject.Helpers
+{
+    public class GoogleMailer
+    {
+        static string email = "aspnet1112011@gmail.com";//vui lòng sửa mail của bạn
+        static string password = "2m@il2011";//vui lòng sửa password của bạn
+
+        // Gửi mail từ hệ thống
+        public static void Send(string to, string subject, string body)
+        {
+            var from = "Administration System <" + email + ">";
+            GoogleMailer.Send(from, to, "", "", subject, body, "");
+        }
+
+        // Gửi mail đơn giản
+        public static void Send(string from, string to, string subject, string body)
+        {
+            GoogleMailer.Send(from, to, "", "", subject, body, "");
+        }
+
+        // Gửi mail nhiều lựa chọn đầy đủ
+        public static void Send(string from, string to, string cc, string bcc, string subject, string body, string attachments)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(from);
+            //mail.ReplyTo = new MailAddress(from);
+            mail.To.Add(new MailAddress(to));
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+            if (!string.IsNullOrEmpty(cc))
+            {
+                mail.CC.Add(cc);
+            }
+            if (!string.IsNullOrEmpty(bcc))
+            {
+                mail.Bcc.Add(bcc);
+            }
+            if (!string.IsNullOrEmpty(attachments))
+            {
+                string[] fileNames = attachments.Split(";,".ToCharArray());
+                foreach (string fileName in fileNames)
+                {
+                    if (fileName.Trim().Length > 0)
+                    {
+                        mail.Attachments.Add(new Attachment(fileName.Trim()));
+                    }
+                }
+            }
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(email, password);
+            client.Send(mail);
+        }
+    }
+}
