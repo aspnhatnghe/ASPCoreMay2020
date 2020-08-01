8dada6262b39023cddf592c84d766ea63afc9fe6
NN <hyhien@gmail.com>
2020-08-01 17:25:27 +0700

    Process cart

--- MyProject/MyProject/Controllers/CartController.cs
+++ MyProject/MyProject/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyProject.Constants;
 using MyProject.DataModels;
 using MyProject.Helpers;
 using MyProject.Models;
@@ -86,7 +87,7 @@ namespace MyProject.Controllers
             if (cartItem != null)
             {
                 myCart.Remove(cartItem);
-            }            
+            }
 
             //update cart
             HttpContext.Session.SetSession("GioHang", myCart);
@@ -98,6 +99,70 @@ namespace MyProject.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProcessCheckout(string NguoiMua, string Email, string DienThoai)
+        {
+            try
+            {
+                _context.Database.BeginTransaction();
+                
+                var khachHang = new KhachHang();
+                if(User.Identity.IsAuthenticated)
+                {
+                    var maKH = User.Claims.FirstOrDefault(p => p.Type == MyClaimTypes.MaKhachHang);
+                    khachHang.MaKhachHang = maKH.Value;
+                }
+                else
+                {
+                    khachHang = new KhachHang
+                    {
+                        MaKhachHang = Guid.NewGuid().ToString(),
+                        TenKhachHang = NguoiMua,
+                        DienThoai = DienThoai,
+                        Email = Email,
+                        TrangThai = false,
+                        VaiTro = Common.Role.Customer
+                    };
+                    _context.Add(khachHang);
+                    _context.SaveChanges();
+                }
+
+                var donHang = new DonHang
+                {
+                    MaKH = khachHang.MaKhachHang,
+                    NgayDat = DateTime.UtcNow,
+                    PhuongThucThanhToan = Common.PaymentMethod.COD,
+                    TrangThaiDatHang = Common.OrderStatus.Open
+                };
+                _context.Add(donHang);
+                _context.SaveChanges();
+
+                foreach(var item in CartItems)
+                {
+                    _context.Add(new ChiTietDonHang { 
+                        MaDonHang = donHang.MaHoaDon,
+                        MaHangHoa = item.Id,
+                        SoLuong = item.SoLuong,
+                        DonGia = item.DonGia
+                    });
+                }
+                _context.SaveChanges();
+
+                HttpContext.Session.Remove("GioHang");
+                _context.Database.CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+            }
+            return View();
         }
     }
 }
